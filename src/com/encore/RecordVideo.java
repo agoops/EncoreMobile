bdfdd2286301d36474ed53abcae17126eeb99b86
Ankit Gupta <ag7bf@virginia.edu>
2013-09-06 22:18:43 -0400

    Added RecordVideo stuff

---
+++ src/com/encore/RecordVideo.java
@@ -0,0 +1,14 @@
+package com.encore;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RecordVideo extends Activity{
+
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.record_video);
+        
+	}
+}
