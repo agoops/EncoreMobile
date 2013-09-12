9b1aba67c068866e5bac0973221130119ae84144
Ankit Gupta <ag7bf@virginia.edu>
2013-09-12 14:01:58 -0400

    Fuckity fuck

--- src/com/encore/CurrentSessions.java
+++ src/com/encore/CurrentSessions.java
@@ -2,13 +2,17 @@ package com.encore;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class CurrentSessions extends Activity {
+	
+	private ListView mListView;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-	
+		setContentView(R.layout.current_sessions);
+		mListView = (ListView) findViewById(R.id.session_list_view);
+		SessionViewAdapter adapter = new SessionViewAdapter(this, null, getPackageManager());
 	
 	}
 }
---
+++ src/com/encore/SessionTemp.java
@@ -0,0 +1,20 @@
+package com.encore;
+
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.webkit.WebView.FindListener;
+
+public class SessionTemp {
+	
+	public SessionTemp () {
+		
+	}
+	
+	public Drawable loadIcon(PackageManager pm){
+		
+		
+		
+		return null;
+		
+	}
+}
