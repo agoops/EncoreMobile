f8009632ceddcc93b39e7be929f28c517579a141
Ankit Gupta <ag7bf@virginia.edu>
2013-12-01 14:31:38 -0500

    Changed around xml layout to accomodate textviews to be added beneath for comments

--- src/com/encore/InboxViewAdapter.java
+++ src/com/encore/InboxViewAdapter.java
@@ -3,7 +3,6 @@ package com.encore;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
@@ -13,22 +12,22 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.encore.API.models.Comment;
 import com.encore.API.models.Session;
 
-public class InboxViewAdapter extends BaseAdapter{
-	
+public class InboxViewAdapter extends BaseAdapter {
+
 	private static String tag = "InboxViewAdapter";
 	private Context mContext;
-    private List<Session> mSessionList;
-    
+	private List<Session> mSessionList;
+
 	public InboxViewAdapter(Context c, ArrayList<Session> list) {
 		mContext = c;
 		mSessionList = list;
 	}
-	
+
 	@Override
 	public int getCount() {
 		return mSessionList.size();
@@ -43,7 +42,7 @@ public class InboxViewAdapter extends BaseAdapter{
 	public long getItemId(int arg0) {
 		return arg0;
 	}
-	
+
 	public void setItemList(ArrayList<Session> list) {
 		this.mSessionList = list;
 	}
@@ -52,49 +51,57 @@ public class InboxViewAdapter extends BaseAdapter{
 	public View getView(int position, View convertView, ViewGroup parent) {
 		// get the selected entry
 		Log.d(tag, "populating position: " + position);
-		//this shouldn't be an applicationInfo object
-        Session entry = (Session) mSessionList.get(position);
- 
-        // reference to convertView
-        SessionView v = (SessionView) convertView;
- 
-        // inflate new layout if null
-        if(v == null) {
-            LayoutInflater inflater = LayoutInflater.from(mContext);
-            v = (SessionView) inflater.inflate(R.layout.inbox_view, parent, false);
-            Button reply = (Button) v.findViewById(R.id.reply);
-            reply.setOnClickListener(new OnClickListener() {
+		// this shouldn't be an applicationInfo object
+		Session entry = (Session) mSessionList.get(position);
+
+		// reference to convertView
+		SessionView v = (SessionView) convertView;
+
+		// inflate new layout if null
+		if (v == null) {
+			LayoutInflater inflater = LayoutInflater.from(mContext);
+			v = (SessionView) inflater.inflate(R.layout.inbox_view, parent,
+					false);
+			Button reply = (Button) v.findViewById(R.id.reply);
+			reply.setOnClickListener(new OnClickListener() {
 
 				@Override
 				public void onClick(View arg0) {
-					
-					//((Activity)mContext).startActivityForResult(new Intent(mContext, AndroidVideoCapture.class), 0);
+
+					// ((Activity)mContext).startActivityForResult(new
+					// Intent(mContext, AndroidVideoCapture.class), 0);
 					SessionView sv = (SessionView) arg0;
 					Session session = sv.getSession();
-					
+
 					Intent intent = new Intent(mContext, StartSession2.class);
 					intent.putExtra("reply", true);
 					intent.putExtra("sessionid", sv.getId());
-					
+
 					mContext.startActivity(intent);
 				}
-            	
-            
-            });
-            v.setSession(entry);
-        }
- 
-        // load controls from layout resources
-        ImageView ivAppIcon = (ImageView)v.findViewById(R.id.ivIcon);
-        TextView tvAppName = (TextView)v.findViewById(R.id.tvName);
- 
-        // set data to display
-//        ivAppIcon.setImageDrawable(entry.loadIcon());
-        tvAppName.setText(entry.getTitle());
- 
-        // return view
-        return v;
+
+			});
+			v.setSession(entry);
+		}
+
+		// load controls from layout resources
+		TextView titleTextView = (TextView) v.findViewById(R.id.tvName);
+		TextView likesTextView = (TextView) v.findViewById(R.id.likes);
+		TextView commentsTextView = (TextView) v.findViewById(R.id.comments);
+
+		// set data to display
+		titleTextView.setText(entry.getTitle());
+		List<Comment> comments = entry.getComments();
+		commentsTextView.setText(comments.size() + " comments");
+		for (int i = 0; i < comments.size(); ++i){
+			TextView oneComment = new TextView(mContext);
+			Log.d(tag, comments.get(i).toString());
+			oneComment.setText(comments.get(i).getText());
+			v.addView(new TextView(mContext));
+			
+		}
+		// return view
+		return v;
 	}
-	
 
 }
